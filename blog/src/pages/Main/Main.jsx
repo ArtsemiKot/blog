b7f4c852723a157b003d4d35151d3d941b4f5857
ArtsemiKot <kot_artemiy@mail.ru>
2024-05-02 20:06:41 +0300

    blog

--- blog/src/pages/Main/Main.jsx
+++ blog/src/pages/Main/Main.jsx
@@ -13,7 +13,27 @@ function Main() {
                     <div className={style.imgVertical2}></div>
                     <div className={style.imgVertical3}></div>
                 </div>
-                <div className={style.contentRight}></div>
+                <div className={style.contentRight}>
+                    <p className={style.textBlog}>BLOG</p>
+                    <div className={style.commentsBlog}>
+                        <div className={style.blockComment1}>
+                            <p className={style.textComment}>Payment methods: Skrill, Neteller, webmoney, Bank transfer</p>
+                            <p className={style.textData}>10.02.2023</p>
+                        </div>
+                        <div className={style.blockComment2}>
+                            <p className={style.textComment}>Payment methods: Skrill, Neteller, webmoney, Bank transfer</p>
+                            <p className={style.textData}>10.02.2023</p>
+                        </div>
+                        <div className={style.blockComment3}>
+                            <p className={style.textComment}>Payment methods: Skrill, Neteller, webmoney, Bank transfer</p>
+                            <p className={style.textData}>10.02.2023</p>
+                        </div>
+                        <div className={style.blockComment4}>
+                            <p className={style.textComment}>Payment methods: Skrill, Neteller, webmoney, Bank transfer</p>
+                            <p className={style.textData}>10.02.2023</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
